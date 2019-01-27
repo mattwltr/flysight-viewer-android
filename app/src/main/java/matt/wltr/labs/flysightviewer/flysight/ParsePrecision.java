d5f5ed89cab9a53456f3bd23a065fa6824d30e8c
Matthias Walter <matt.wltr@gmail.com>
2019-01-27 20:58:35 +0100

    * added javadoc to ParsePrecision

--- app/src/main/java/matt/wltr/labs/flysightviewer/flysight/ParsePrecision.java
+++ app/src/main/java/matt/wltr/labs/flysightviewer/flysight/ParsePrecision.java
@@ -1,7 +1,16 @@
 package matt.wltr.labs.flysightviewer.flysight;
 
 public enum ParsePrecision {
+    /**
+     * parse all records
+     */
     ALL,
+    /**
+     * skip records if they're closer than 1 second
+     */
     EACH_SECOND,
+    /**
+     * skip records if they're closer than 1 second outside the exit-opening range
+     */
     INTELLIGENT
 }
